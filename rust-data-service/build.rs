7d7e99cf830b45985a65e4b2966501d74eef2265
Chun-Yi Chen <cge123321@gmail.com>
2026-07-14 00:50:49 +0200

    feat(rithmic): add local protobuf build boundary

---
+++ rust-data-service/build.rs
@@ -0,0 +1,55 @@
+use std::{env, path::PathBuf};
+
+const RITHMIC_V2_PROTOS: &[&str] = &[
+    "request_rithmic_system_info.proto",
+    "response_rithmic_system_info.proto",
+    "request_login.proto",
+    "response_login.proto",
+    "request_logout.proto",
+    "response_logout.proto",
+    "request_heartbeat.proto",
+    "response_heartbeat.proto",
+    "reject.proto",
+    "forced_logout.proto",
+    "request_market_data_update.proto",
+    "response_market_data_update.proto",
+    "request_front_month_contract.proto",
+    "response_front_month_contract.proto",
+    "last_trade.proto",
+    "best_bid_offer.proto",
+    "request_time_bar_replay.proto",
+    "response_time_bar_replay.proto",
+    "time_bar.proto",
+];
+
+fn main() {
+    println!("cargo:rerun-if-env-changed=RITHMIC_PROTO_DIR");
+
+    if env::var_os("CARGO_FEATURE_RITHMIC").is_none() {
+        return;
+    }
+
+    let proto_dir = env::var_os("RITHMIC_PROTO_DIR")
+        .map(PathBuf::from)
+        .filter(|path| path.is_dir())
+        .expect("RITHMIC_PROTO_DIR must point to the local Rithmic proto directory");
+    let protos: Vec<_> = RITHMIC_V2_PROTOS
+        .iter()
+        .map(|name| proto_dir.join(name))
+        .collect();
+
+    for proto in &protos {
+        if !proto.is_file() {
+            panic!("missing required Rithmic proto: {}", proto.display());
+        }
+        println!("cargo:rerun-if-changed={}", proto.display());
+    }
+
+    let protoc = protoc_bin_vendored::protoc_bin_path()
+        .expect("vendored protoc is unavailable for this platform");
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc);
+    config
+        .compile_protos(&protos, &[proto_dir])
+        .expect("failed to compile local Rithmic protos");
+}
---
+++ rust-data-service/src/connector/rithmic/mod.rs
@@ -0,0 +1,24 @@
+#[allow(dead_code, clippy::enum_variant_names, clippy::tabs_in_doc_comments)]
+pub(crate) mod protocol {
+    include!(concat!(env!("OUT_DIR"), "/rti.rs"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::protocol::{RequestHeartbeat, RequestLogin};
+
+    #[test]
+    fn generated_v2_protocol_contains_login_and_heartbeat_messages() {
+        let login = RequestLogin {
+            template_id: 10,
+            ..Default::default()
+        };
+        let heartbeat = RequestHeartbeat {
+            template_id: 18,
+            ..Default::default()
+        };
+
+        assert_eq!(login.template_id, 10);
+        assert_eq!(heartbeat.template_id, 18);
+    }
+}
