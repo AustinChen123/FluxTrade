cc501da8fa916bb7f6081c2a1d6cbc532832c445
Chun-Yi Chen <cge123321@gmail.com>
2026-08-13 08:46:36 +0200

    refactor(runtime): own task supervision

--- rust-data-service/src/connector/live_runtime.rs
+++ rust-data-service/src/connector/live_runtime.rs
@@ -1,4 +1,4 @@
-use crate::{live_event_pipeline::AggregationSourceEvent, model, TaskId};
+use crate::{live_event_pipeline::AggregationSourceEvent, model, runtime_supervisor::TaskId};
 use anyhow::Result;
 use std::collections::HashSet;
 use tokio::{sync::mpsc, task::JoinSet};
